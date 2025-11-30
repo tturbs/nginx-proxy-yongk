be90f6e5b030043bd90d0ab9d750b481cfb9262b
Yongk Kim <tturbs@hotmail.com>
2025-11-30 21:57:32 +0900

    fix: commuzz.duckdns.org 502 error

--- certbot/run.sh
+++ certbot/run.sh
@@ -2,11 +2,32 @@
 
 set -e
 
+# DuckDNS credentials file
 CREDENTIALS_FILE="/etc/letsencrypt/.secrets/duckdns.ini"
 
-# Initial certificate issuance is handled by the first run of this script.
-# This loop is for automatic renewal.
+# Domains
+DOMAINS=(
+  "basketball-scoreboard.duckdns.org"
+  "commuzz.duckdns.org"
+)
 
+# Join domains for certbot command
+CERT_DOMAINS=""
+for DOMAIN in "${DOMAINS[@]}"; do
+  CERT_DOMAINS="${CERT_DOMAINS}-d ${DOMAIN} "
+done
+
+# Issue or renew certificate
+certbot certonly \
+  --dns-duckdns \
+  --dns-duckdns-credentials "${CREDENTIALS_FILE}" \
+  --email yonguk.ids@gmail.com \
+  --agree-tos \
+  --no-eff-email \
+  --expand \
+  ${CERT_DOMAINS}
+
+# Renewal loop
 while true; do
     echo "Renewing certificates..."
     certbot renew --quiet
